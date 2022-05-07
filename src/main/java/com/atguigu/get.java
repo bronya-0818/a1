485f7e3480fcf67fb988b9080716356579af8127
linglin <linglin@qq.com>
2022-05-07 08:59:05 +0800

    push test

--- src/main/java/com/atguigu/get.java
+++ src/main/java/com/atguigu/get.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class get {
     public static void main(String[] args) {
-        System.out.println("hello,world");
+        System.out.println("hello,world，git");
     }
 }
